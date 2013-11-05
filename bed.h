eb82b3bc018973965ee533309f7c51a625ea5dd3
Dr. Ahmed AKL <ahmed.project123@gmail.com>
2013-11-05 12:22:15 +0200

    Create bed.h

---
+++ bed.h
@@ -0,0 +1,23 @@
+/*
+  bed.h - Library for flashing Morse code.
+  Created by ....(Actual contributors)...... , November 15, 2013.
+  Released into the public domain.
+*/
+
+#ifndef bed_h
+#define bed_h
+
+#include "Arduino.h"
+
+class bed
+{
+  public:
+  // insert public components
+  // i.e. bed(int pin);
+    
+  private:
+  // insert private components
+  // int _pin;
+};
+
+#endif
